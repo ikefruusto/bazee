5282a907d1dc6632c5c06916a18a7a937250fb6b
nikania <nikania1988@gmail.com>
2014-02-01 19:45:14 -0800

    the ball is now moving back and forth

--- bazee.js
+++ bazee.js
@@ -1,4 +1,114 @@
+// This file contains the code for Pong (the game). 
+// February 2014
+
+// The requestAnimationFrame is used instead of just setTimeout for optimization purposes. For example, 
+// calls won't be sent until a browser tab is active
+var animate = window.requestAnimationFrame ||
+	function(callback) {window.setTimeout(callback, 1000/60) };
+	
+	
+// getting 2D context of the canvas element
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillRect(canvas.width / 2 - 50, canvas.height / 2 - 50, 100, 100);
+// attaching the canvas to the screen and starting the animation when the page loads
+window.onload = function() {
+	document.body.appendChild(canvas);
+	animate(move);
+};
+
+// The move function will do the following:
+//  - updating three objects: the ball, player1's paddle, and player2's paddle
+//  - rendering the above objects
+//  - calling the move object again
+var move = function() {
+	update();
+	render();
+	animate(move);
+};
+
+// setting the background color and rendering the ball and the players
+var render = function() {
+	context.fillStyle = "#FFFFFF";
+	context.fillRect(0, 0, canvas.width, canvas.height);
+	player1.render();
+	player2.render();
+	ball.render();
+}
+
+// the update function
+var update = function() {
+	ball.update(player1.paddle, player2.paddle);
+};
+
+Ball.prototype.update = function(paddle1, paddle2) {
+  this.x += this.x_speed;
+  this.y += this.y_speed;
+  var top_x = this.x - 8;
+  var top_y = this.y - 8;
+
+  if(this.x - 8 < 8) { 
+    this.x = 16;
+    this.x_speed = -this.x_speed;
+  } else if(this.x + 8 > 792) { 
+    this.x = 784;
+    this.x_speed = -this.x_speed;
+  }
+};
+
+// Creating the Ball object
+// the x,y coordinates represent the center of the circle and the radius is 10
+function Ball(x, y) {
+  this.x = x;
+  this.y = y;
+  this.x_speed = 5;
+  this.y_speed = 0;
+  this.radius = 8;
+}
+
+Ball.prototype.render = function() {
+  context.beginPath();
+  context.arc(this.x, this.y, this.radius, 2 * Math.PI, false);
+  context.fillStyle = "#FF0000";
+  context.fill();
+};
+
+// Creating the Paddle object
+function Paddle(x, y, width, height) {
+	this.x = x;
+	this.y = y;
+	this.width = width;
+	this.height = height;
+	this.x_speed = 0;
+	this.y_speed = 0;
+}
+
+Paddle.prototype.render = function() {
+  context.fillStyle = "#000000";
+  context.fillRect(this.x, this.y, this.width, this.height);
+};
+
+// creating the paddle object for player1 and rendering it
+function Player1() {
+	this.paddle = new Paddle(0, 250, 15, 80);
+}
+
+Player1.prototype.render = function() {
+  this.paddle.render();
+};
+
+// crating the paddle object for player2 and rendering it
+function Player2() {
+	this.paddle = new Paddle(785, 250, 15, 80);
+}
+
+Player2.prototype.render = function() {
+  this.paddle.render();
+};
+	
+// creating the ball, player1, and player2 objects
+var ball = new Ball(200, 300);
+var player1 = new Player1();
+var player2 = new Player2();
+
+	
